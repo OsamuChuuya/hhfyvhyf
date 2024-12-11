693a97e12e9d81f96fcb63a3dd9dc887edd80e89
OsamuChuuya <osamuchuuya26@gmail.com>
2024-12-11 22:37:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var rtute = 123;
+let ser = 46;
+const ndr = 859;
+
+var userName = prompt('vedite ima:');
+
+var userName = prompt('familia:');
+var userName = prompt('yosh:');
+var userName = prompt('adres:');
+
+alert('HUSH KEMABSIZ')
